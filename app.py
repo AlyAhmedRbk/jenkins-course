5d69d4439b278393247b0f648cc2d038f87606a2
Ali Ahmed <alyahmedrbk@gmail.com>
2025-01-10 16:27:17 +0500

    Jenkinsfile added

--- app.py
+++ app.py
@@ -12,3 +12,15 @@ def sub(a, b):
     print(a-b)
 
 sub(5, 3)
+
+arr = [1, 2, 4, 3, 5]
+
+def sumOfArr(arr):
+    sum=0
+
+    for item in arr:
+        sum+=item
+    
+    return sum
+
+print(sumOfArr(arr))
